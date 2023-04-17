06268844a2224db001516eaa9fdb8034b8cfad9a
eito-fis <eamon@itofisher.com>
2023-04-17 17:00:54 -0400

    New colors

--- js/fragment_shaders.js
+++ js/fragment_shaders.js
@@ -3,8 +3,8 @@ in float d;
 
 uniform float amplitude;
 
-vec3 colorA = vec3(0.149,0.141,0.912);
-vec3 colorB = vec3(1.000,0.833,0.224);
+vec3 colorA = vec3(0.1,0.5,0.1);
+vec3 colorB = vec3(1.0, 1.0, 1.0);
 
 void main() {
   float perc = (d + amplitude) / (amplitude * 2.0);
