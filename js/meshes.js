9f464ba0a7e2e4007992a1af6bc84011cca6a9fc
eito-fis <eamon@itofisher.com>
2023-04-20 16:56:06 -0400

    Make shaders folder

--- js/meshes.js
+++ js/meshes.js
@@ -2,8 +2,8 @@ import * as THREE from "three";
 import { createNoise2D } from "simplex-noise";
 import alea from "alea";
 
-import { fragmentShader } from "./fragment_shaders";
-import { vertexShader } from "./vertex_shaders";
+import { fragmentShader } from "./shaders/fragment_shaders";
+import { vertexShader } from "./shaders/vertex_shaders";
 
 let scene;
 let meshes = {};
